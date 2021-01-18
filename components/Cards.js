bd53e3c1f00b904650c57646a5401022662f7762
Guadalupe Brenes <brenesmesa@gmail.com>
2021-01-18 11:51:14 -0600

    Rendering API

--- components/Cards.js
+++ components/Cards.js
@@ -1,92 +1,26 @@
 import React from 'react'
 
-const Cards = () => {
+const Cards = ({movies}) => {
+
+  const renderMovies = movies => {
+    return movies?.map(movie => (
+      <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
+        <div className="card h-100">
+          <a href="#"><img className="card-img-top" src={movie.image} alt="" /></a>
+          <div className="card-body">
+            <h4 className="card-title"><a href="#">{movie.name}</a></h4>
+            <h5>{movie.releaseYear}</h5>
+            <p className="card-text">{movie.description}</p>
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+          </div>
+        </div>
+      </div>))
+  } 
   return (
     <>
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title"><a href="#">Item One</a></h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title"><a href="#">Item One</a></h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title"><a href="#">Item One</a></h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title"><a href="#">Item One</a></h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title"><a href="#">Item One</a></h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title"><a href="#">Item One</a></h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
+      { renderMovies(movies)}
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import SideMenu from '../components/SideMenu';
 import Carousel from '../components/Carousel';
 import Cards from '../components/Cards';
 import Footer from '../components/Footer';
+import { getMovieData } from './api/movies'
 
 export default function Home() {
+
+  // Ther's different ways to get the info from the API:  // 
+  // 1. Usign useState and useEffect
+
+  const [movies, setMovies] = useState([])
+    
+  useEffect(() => {
+    const fetchMovies = async () => {
+     const movieData = await getMovieData()
+     setMovies(movieData)
+    } 
+    fetchMovies()
+  }, [])
+  
+
   return (
     <div>
       <Head>
         <title>Home</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"/>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous"/>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossOrigin="anonymous"></script>
       </Head>
 
       <Navbar/>
@@ -25,7 +42,7 @@ export default function Home() {
           <div className="col-lg-9">
             <Carousel/>
             <div className="row">
-              <Cards/>
+              <Cards movies={movies}/>
             </div>
           </div>
         </div>
@@ -34,3 +51,9 @@ export default function Home() {
     </div>
   )
 }
+
+// // 2. From getIntialPorps
+// Home.getInitialProps = async () => {
+//   const movies = await getMovieData()
+//   return movies
+// }
